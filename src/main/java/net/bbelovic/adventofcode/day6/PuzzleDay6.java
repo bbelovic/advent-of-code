b73e178382dae60f72ae10126f2e5304b9505f73
bbelovic <bbelovic@gmail.com>
2018-07-04 23:05:21 +0200

    Continuing on day 6

--- src/main/java/net/bbelovic/adventofcode/day6/PuzzleDay6.java
+++ src/main/java/net/bbelovic/adventofcode/day6/PuzzleDay6.java
@@ -3,9 +3,13 @@ package net.bbelovic.adventofcode.day6;
 public class PuzzleDay6 {
     private final boolean [][] grid = new boolean[1000][1000];
     public long solve() {
+
+        final var coords1 = new int [] {499, 499};
+        final var coords2 = new int [] {500, 500};
+
         var cnt = 0L;
-        for (var i = 0; i <= 0; i++) {
-            for (var j = 0; j <= 999; j++) {
+        for (var i = coords1[0]; i <= coords1[1]; i++) {
+            for (var j = coords2[1]; j <= coords2[0]; j++) {
                 grid [i][j] = true;
                 ++cnt;
             }

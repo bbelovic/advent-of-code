38a2227074a137e3e54ae86391be9512b4dfaed5
bbelovic <bbelovic@gmail.com>
2018-07-06 14:01:16 +0200

    Added one more test case

--- src/main/java/net/bbelovic/adventofcode/day6/PuzzleDay6.java
+++ src/main/java/net/bbelovic/adventofcode/day6/PuzzleDay6.java
@@ -2,14 +2,11 @@ package net.bbelovic.adventofcode.day6;
 
 public class PuzzleDay6 {
     private final boolean [][] grid = new boolean[1000][1000];
-    public long solve() {
-
-        final var coords1 = new int [] {499, 499};
-        final var coords2 = new int [] {500, 500};
+    public long solve(int[] coords1, int[] coords2) {
 
         var cnt = 0L;
-        for (var i = coords1[0]; i <= coords1[1]; i++) {
-            for (var j = coords2[1]; j <= coords2[0]; j++) {
+        for (var i = coords1[1]; i <= coords2[1]; i++) {
+            for (var j = coords1[0]; j <= coords2[0]; j++) {
                 grid [i][j] = true;
                 ++cnt;
             }
--- src/test/java/net/bbelovic/adventofcode/day6/PuzzleDay6Test.java
+++ src/test/java/net/bbelovic/adventofcode/day6/PuzzleDay6Test.java
@@ -2,13 +2,38 @@ package net.bbelovic.adventofcode.day6;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
 public class PuzzleDay6Test {
+    private int [] coords1;
+    private int [] coords2;
+    private int expectedLightsOnCount;
+
+    public PuzzleDay6Test(int[] coords1, int[] coords2, int expectedLightsOnCount) {
+        this.coords1 = coords1;
+        this.coords2 = coords2;
+        this.expectedLightsOnCount = expectedLightsOnCount;
+    }
+
     @Test
     public void test() {
-        long actual = new PuzzleDay6().solve();
-        long expected = 1000 ;
-        Assert.assertEquals(expected, actual);
+        long actual = new PuzzleDay6().solve(coords1, coords2);
+        Assert.assertEquals(expectedLightsOnCount, actual);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testData() {
+        return List.of(new Object[][] {
+                {new int []{0,0}, new int [] {999,0}, 1000},
+                {new int []{0,0}, new int [] {999,999}, 1_000_000},
+                {new int []{499,499}, new int [] {500,500}, 4},
+                {new int []{0,0}, new int [] {2,2}, 9},
+        });
     }
 
 }

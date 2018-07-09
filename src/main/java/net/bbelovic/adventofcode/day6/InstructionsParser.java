c7cd124b6eaf13cf274e78c83e5247332fa61780
bbelovic <bbelovic@gmail.com>
2018-07-09 22:15:17 +0200

    Improved instruction parser.

--- src/main/java/net/bbelovic/adventofcode/day6/InstructionsParser.java
+++ src/main/java/net/bbelovic/adventofcode/day6/InstructionsParser.java
@@ -2,6 +2,17 @@ package net.bbelovic.adventofcode.day6;
 
 final class InstructionsParser {
     public Instructions parseInstructions(String line) {
+        String[] parts = line.split(" ");
+        if (parts.length == 5) {
+            var coords1Part = parts[2];
+            var coords2Part= parts[4];
+            var coords1 = coords1Part.split(",");
+            var coords2 = coords2Part.split(",");
+            return new Instructions(
+                    Integer.parseInt(coords1[0]),
+                    Integer.parseInt(coords1[1]),
+                    Integer.parseInt(coords2[0]), Integer.parseInt(coords2[1]));
+        }
         return new Instructions(0, 0, 0, 0);
     }
 }
--- src/test/java/net/bbelovic/adventofcode/day6/InstructionsParserTest.java
+++ src/test/java/net/bbelovic/adventofcode/day6/InstructionsParserTest.java
@@ -1,20 +1,47 @@
 package net.bbelovic.adventofcode.day6;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class InstructionsParserTest {
+    private final String inputLine;
+    private final int x1;
+    private final int y1;
+    private final int x2;
+    private final int y2;
+
+    public InstructionsParserTest(String inputLine, int x1, int y1, int x2, int y2) {
+        this.inputLine = inputLine;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
     @Test
     public void
     should_parse_instruction_from_string() {
-        var x1 = "turn on 606,361 through 892,600";
-        var x2 = "turn off 448,208 through 645,684";
-        var x3 = "toggle 50,472 through 452,788";
         var parser = new InstructionsParser();
-        var expected = new Instructions(606, 361, 892, 600);
-        var actual = parser.parseInstructions(x1);
+        var expected = new Instructions(x1, y1, x2, y2);
+        var actual = parser.parseInstructions(inputLine);
         assertEquals(expected, actual);
     }
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> testData() {
+        return List.of(
+                new Object[][] {
+                        {"turn on 606,361 through 892,600", 606, 361, 892, 600},
+                        {"turn off 448,208 through 645,684", 448, 208, 645, 684},
+                        {"toggle 50,472 through 452,788", 50, 472, 452, 788},
+                }
+        );
+    }
 }

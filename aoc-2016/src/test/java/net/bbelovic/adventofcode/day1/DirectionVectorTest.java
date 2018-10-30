1ee8d10b68f4d6c1c8a36ed7404ac1f714fa5c90
bbelovic <bbelovic@gmail.com>
2018-10-30 22:16:14 +0100

    Added test for turning in other (right) direction

--- aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/DirectionVectorTest.java
+++ aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/DirectionVectorTest.java
@@ -3,6 +3,7 @@ package net.bbelovic.adventofcode.day1;
 import org.junit.jupiter.api.Test;
 
 import static net.bbelovic.adventofcode.day1.DirectionVector.Direction.Left;
+import static net.bbelovic.adventofcode.day1.DirectionVector.Direction.Right;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DirectionVectorTest {
@@ -19,4 +20,16 @@ class DirectionVectorTest {
         assertEquals(expected, start.turn(Left).turn(Left).turn(Left).turn(Left));
     }
 
+    @Test
+    void should_turn_right() {
+        var start = new DirectionVector(0, 1);
+        var expected = new DirectionVector(1, 0);
+        assertEquals(expected, start.turn(Right));
+        expected = new DirectionVector(0, -1);
+        assertEquals(expected, start.turn(Right).turn(Right));
+        expected = new DirectionVector(-1, 0);
+        assertEquals(expected, start.turn(Right).turn(Right).turn(Right));
+        expected = new DirectionVector(0, 1);
+        assertEquals(expected, start.turn(Right).turn(Right).turn(Right).turn(Right));
+    }
 }

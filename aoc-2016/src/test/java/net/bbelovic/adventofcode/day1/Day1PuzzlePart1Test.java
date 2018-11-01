9f978be14b54bbfca51caab6b8cd90ee2dd9b927
bbelovic <bbelovic@gmail.com>
2018-11-01 21:59:58 +0100

    Extended tests

--- aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart1Test.java
+++ aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart1Test.java
@@ -1,15 +1,22 @@
 package net.bbelovic.adventofcode.day1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day1PuzzlePart1Test {
-    @Test
-    void should_compute_distance_from_start() {
-        var input = "R2,L3";
-        long expectedDistance = 5L;
+    @ParameterizedTest
+    @MethodSource("testData")
+    void should_compute_distance_from_start(String input, int expectedDistance) {
         long actualDistance = new Day1PuzzlePart1().solve(input);
         assertEquals(expectedDistance, actualDistance);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(Arguments.of("R2,L3", 5), Arguments.of("R2,R2,R2", 2), Arguments.of("R5,L5,R5,R3", 12));
     }
 }

1625fb843005b39f81fab0b6588f38ead5e3f362
bbelovic <bbelovic@gmail.com>
2018-11-02 19:37:17 +0100

    Using infix notation in Position class.

--- aoc-2016/src/main/kotlin/net/bbelovic/adventofcode/day1/Day1PuzzlePart1.kt
+++ aoc-2016/src/main/kotlin/net/bbelovic/adventofcode/day1/Day1PuzzlePart1.kt
@@ -9,18 +9,18 @@ class Day1PuzzlePart1 : Puzzle<String, Long> {
     override fun solve(input: String): Long {
         val list = input.split(", ")
 
-        var start = Position(Point(0, 1))
+        var actualPosition = Position(Point(0, 1))
 
         for (inp in list) {
             val directionCode = inp.first()
             val steps = inp.last().toString().toInt()
-            start = when (directionCode) {
-                'L' -> start.turn(Left).moveBy(steps)
-                'R' -> start.turn(Right).moveBy(steps)
-                else -> throw IllegalArgumentException("Unknown input '$input'")
+            actualPosition = when (directionCode) {
+                'L' -> actualPosition turn Left moveBy steps
+                'R' -> actualPosition turn Right  moveBy steps
+                else -> throw IllegalArgumentException("Unknown input [$input]")
             }
         }
-        return distance(start.value)
+        return distance(actualPosition.value)
     }
 
     private fun distance(p: Point): Long {
--- aoc-2016/src/main/kotlin/net/bbelovic/adventofcode/day1/Position.kt
+++ aoc-2016/src/main/kotlin/net/bbelovic/adventofcode/day1/Position.kt
@@ -1,8 +1,8 @@
 package net.bbelovic.adventofcode.day1
 
 data class Position(val orientation: Point, val value: Point = Point(0,0)) {
-    fun turn(direction: Direction) = Position(orientation.turn(direction), value)
-    fun moveBy(steps: Int): Position {
+    infix fun turn(direction: Direction) = Position(orientation.turn(direction), value)
+    infix fun moveBy(steps: Int): Position {
         val newX = this.value.x + this.orientation.x * steps
         val newY = this.value.y + this.orientation.y * steps
         return Position(orientation, Point(newX, newY))

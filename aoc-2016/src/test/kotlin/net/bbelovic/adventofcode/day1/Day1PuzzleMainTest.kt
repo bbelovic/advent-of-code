d5beef0fe3e20fead66254b253fcac755826d487
bbelovic <bbelovic@gmail.com>
2018-11-06 21:49:44 +0100

    Updated JUnit 5 dependency, polished tests for day 1.

--- aoc-2016/src/test/kotlin/net/bbelovic/adventofcode/day1/Day1PuzzleMainTest.kt
+++ aoc-2016/src/test/kotlin/net/bbelovic/adventofcode/day1/Day1PuzzleMainTest.kt
@@ -1,16 +1,23 @@
 package net.bbelovic.adventofcode.day1
 
 import net.bbelovic.adventofcode.InputReader
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import net.bbelovic.adventofcode.Puzzle
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class Day1PuzzleMainTest {
-    @Test
-    fun should_solve_puzzle() {
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun should_solve_puzzle(puzzle: Puzzle<String, Long>, expectedResult: Long) {
         val inputString = InputReader().readFileIntoString("input1.txt")
-        val puzzle = Day1PuzzlePart1()
-        val expectedResult = 332L
         val actualResult = puzzle.solve(inputString)
-        Assertions.assertEquals(expectedResult, actualResult)
+        assertEquals(expectedResult, actualResult)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf<Arguments>(Arguments.of(Day1PuzzlePart1(), 332))
     }
 }

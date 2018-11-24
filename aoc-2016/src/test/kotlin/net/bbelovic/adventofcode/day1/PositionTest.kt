9bc832bacb7d0e2730db4e3893e99b89c07cba2f
bbelovic <bbelovic@gmail.com>
2018-11-24 23:03:35 +0100

    Fixed failing test

--- aoc-2016/src/test/kotlin/net/bbelovic/adventofcode/day1/PositionTest.kt
+++ aoc-2016/src/test/kotlin/net/bbelovic/adventofcode/day1/PositionTest.kt
@@ -17,7 +17,7 @@ class PositionTest {
     @Test
     fun should_move_number_of_steps_in_direction() {
         val position = Position(orientation = Point(0, 1))
-        val expected = Position(Point(0, 1), Point(0, 3))
+        val expected = Position(Point(0, 1), Point(0, 3), listOf(Point(0, 1), Point(0, 2), Point(0, 3)))
         val actual = position moveBy 3
         assertEquals(expected, actual)
     }

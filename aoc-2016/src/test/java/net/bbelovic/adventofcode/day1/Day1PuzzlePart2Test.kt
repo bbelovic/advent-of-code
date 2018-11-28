b8544b1ad3a9c3edd4badfcc2b65225331d07756
bbelovic <bbelovic@gmail.com>
2018-11-28 22:31:09 +0100

    Improving test coverage

--- aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart2Test.kt
+++ aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart2Test.kt
@@ -1,6 +1,7 @@
 package net.bbelovic.adventofcode.day1
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
@@ -8,11 +9,14 @@ import org.junit.jupiter.params.provider.MethodSource
 class Day1PuzzlePart2Test {
     @ParameterizedTest
     @MethodSource("testData")
-    fun should_compute_distance_to_first_location_visited_twice(input: String,
-                                                                expectedResult: Long) {
+    fun should_compute_distance_to_first_location_visited_twice(input: String, expectedResult: Long) {
         val puzzle = Day1PuzzlePart2()
         val actualResult = puzzle.solve(input)
         assertEquals(expectedResult, actualResult)
+    }
+
+    @Test
+    fun should_throw_exception_when_no_position_was_visited_twice() {
 
     }
 

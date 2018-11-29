4ea5c8000e07363627e2bb6d5a2a64acdc03a274
bbelovic <bbelovic@gmail.com>
2018-11-29 22:14:18 +0100

    Simplified tests

--- aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart2Test.kt
+++ aoc-2016/src/test/java/net/bbelovic/adventofcode/day1/Day1PuzzlePart2Test.kt
@@ -3,30 +3,17 @@ package net.bbelovic.adventofcode.day1
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 
 class Day1PuzzlePart2Test {
-    @ParameterizedTest
-    @MethodSource("testData")
-    fun should_compute_distance_to_first_location_visited_twice(input: String, expectedResult: Long) {
-        val puzzle = Day1PuzzlePart2()
-        val actualResult = puzzle.solve(input)
-        assertEquals(expectedResult, actualResult)
+    private val puzzle = Day1PuzzlePart2()
+    @Test
+    fun should_compute_distance_to_first_location_visited_twice() {
+        val actualResult = puzzle.solve("R8, R4, R4, R8")
+        assertEquals(4L, actualResult)
     }
 
     @Test
     fun should_throw_exception_when_no_position_was_visited_twice() {
-        val input = "R8, R4, R4"
-        assertThrows(IllegalStateException().javaClass) {Day1PuzzlePart2().solve(input)}
+        assertThrows(IllegalStateException().javaClass) {puzzle.solve("R8, R4")}
     }
-
-    companion object {
-        @JvmStatic
-        fun testData() = listOf<Arguments>(
-                Arguments.of("R8, R4, R4, R8", 4L)
-        )
-    }
-
 }

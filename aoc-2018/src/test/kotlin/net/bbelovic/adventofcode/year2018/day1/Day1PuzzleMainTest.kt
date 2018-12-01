2e499cf5ad81bca106267e01947dd11bc6e7cfc0
bbelovic <bbelovic@gmail.com>
2018-12-01 14:37:51 +0100

    Implementedpart 1 od day 1 puzzle

---
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day1/Day1PuzzleMainTest.kt
@@ -0,0 +1,23 @@
+package net.bbelovic.adventofcode.year2018.day1
+
+import net.bbelovic.adventofcode.InputReader
+import net.bbelovic.adventofcode.Puzzle
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class Day1PuzzleMainTest {
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun should_solve_puzzle(puzzle: Puzzle<String, Long>, expectedResult: Long) {
+        val string = InputReader().readAllLines("input.txt").joinToString(", ")
+        val actualResult = puzzle.solve(string)
+        assertEquals(expectedResult, actualResult)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(Arguments.of(Day1PuzzlePart1(), 430L))
+    }
+}

9a9ff48f3d4c5f625c88af41785a959205132b10
bbelovic <bbelovic@gmail.com>
2018-12-02 13:56:03 +0100

    Polish

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day1/Day1PuzzlePart2.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day1/Day1PuzzlePart2.kt
@@ -4,31 +4,24 @@ import net.bbelovic.adventofcode.Puzzle
 
 class Day1PuzzlePart2 : Puzzle<String, Long> {
     override fun solve(input: String): Long {
-      var notFound = true
-        var init = 0L
-
-
-
-
-        val frequencies = mutableSetOf(init)
-        val list = input.splitToSequence(", ").map { it -> it.toLong() }.toList()
+        var notFound = true
+        var startingFrequency = 0L
+        val knownFrequencies = mutableSetOf(startingFrequency)
+        val frequencyList = input.splitToSequence(", ").map { it -> it.toLong() }.toList()
         var result = Long.MIN_VALUE
         while (notFound) {
-          for (f in list) {
-              val newFreq = init + f
-              if (newFreq in frequencies) {
-                  result = newFreq
-                  notFound = false
-                  break
-              } else {
-                  frequencies.add(newFreq)
-              }
-              init = newFreq
-
-          }
-
-      }
-
-      return result
+            for (frequency in frequencyList) {
+                val candidate = startingFrequency + frequency
+                if (candidate in knownFrequencies) {
+                    result = candidate
+                    notFound = false
+                    break
+                } else {
+                    knownFrequencies.add(candidate)
+                }
+                startingFrequency = candidate
+            }
+        }
+        return result
     }
 }

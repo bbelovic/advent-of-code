17279211c4adb0d5e216ede4e3342205b17dd400
bbelovic <bbelovic@gmail.com>
2018-12-05 22:05:49 +0100

    Started with part 2 of day 2 puzzle

---
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2.kt
@@ -0,0 +1,9 @@
+package net.bbelovic.adventofcode.year2018.day2
+
+import net.bbelovic.adventofcode.Puzzle
+
+class Day2PuzzlePart2 : Puzzle<List<String>, String> {
+    override fun solve(input: List<String>): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2Test.kt
@@ -0,0 +1,53 @@
+package net.bbelovic.adventofcode.year2018.day2
+
+import net.bbelovic.adventofcode.Puzzle
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.lang.StringBuilder
+
+class Day2PuzzlePart2Test {
+    @Test
+    fun `should return common letters`() {
+        val puzzle: Puzzle<List<String>, String> = Day2PuzzlePart2()
+        val inputs = listOf("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz")
+        val actualResult = puzzle.solve(inputs)
+        Assertions.assertEquals("fgij", actualResult)
+    }
+
+    @Test
+    fun `should return common_letters`() {
+        var actual = findCommonLetters("fghij", "fguij")
+        assertEquals("fgij", actual)
+        actual = findCommonLetters("abcde", "axcye")
+        assertEquals("ace", actual)
+    }
+
+    private fun findCommonLetters(first: String, second: String): String {
+        val result = StringBuilder()
+        for (i in 0 until first.length) {
+            if (first[i] == second[i]) result.append(first[i])
+        }
+        return result.toString()
+    }
+
+    @Test
+    fun `should find strings with single-letter difference`() {
+        val actual = hasSingleDifference("fghij", "fguij")
+        assertTrue(actual)
+        assertFalse(hasSingleDifference("abcde", "axcye"))
+        assertFalse(hasSingleDifference("abcde", "abcde"))
+        assertFalse(hasSingleDifference("", ""))
+    }
+
+    private fun hasSingleDifference(first: String, second: String): Boolean {
+        var diffs = 0
+        for (i in 0..(first.length - 1) ) {
+            if (first[i] != second[i]) {
+                ++diffs
+            }
+            if (diffs >= 2) break
+        }
+        return diffs == 1
+    }
+}

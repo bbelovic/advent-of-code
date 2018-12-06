1131d0fb6bda5f388c7bae1547db88dd36ccad93
bbelovic <bbelovic@gmail.com>
2018-12-06 21:37:27 +0100

    first implementation of part 2

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2.kt
@@ -1,9 +1,44 @@
 package net.bbelovic.adventofcode.year2018.day2
 
 import net.bbelovic.adventofcode.Puzzle
+import java.lang.StringBuilder
+import java.util.ArrayList
 
 class Day2PuzzlePart2 : Puzzle<List<String>, String> {
     override fun solve(input: List<String>): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        for (each in input) {
+            val copy = ArrayList(input - each)
+            for (s in copy) {
+                if (!hasSingleDifference(each, s)) {
+                    continue
+                } else {
+                    return findCommonLetters(each, s)
+                }
+            }
+        }
+        throw IllegalStateException("Expected exactly one pair of IDs that differ in one letter.")
+    }
+
+    private fun hasSingleDifference(first: String, second: String): Boolean {
+        var diffs = 0
+        for (i in 0 until first.length) {
+            if (first[i] != second[i]) {
+                ++diffs
+            }
+            if (diffs >= 2) break
+        }
+        return diffs == 1
+    }
+
+    private fun findCommonLetters(first: String, second: String): String {
+        val result = StringBuilder()
+        for (i in 0 until first.length) {
+            if (first[i] == second[i]) result += first[i]
+        }
+        return result.toString()
+    }
+
+    operator fun StringBuilder.plusAssign(c: Char) {
+        this.append(c)
     }
 }
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2Test.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day2/Day2PuzzlePart2Test.kt
@@ -1,10 +1,8 @@
 package net.bbelovic.adventofcode.year2018.day2
 
 import net.bbelovic.adventofcode.Puzzle
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.lang.StringBuilder
 
 class Day2PuzzlePart2Test {
     @Test
@@ -12,46 +10,6 @@ class Day2PuzzlePart2Test {
         val puzzle: Puzzle<List<String>, String> = Day2PuzzlePart2()
         val inputs = listOf("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz")
         val actualResult = puzzle.solve(inputs)
-        Assertions.assertEquals("fgij", actualResult)
-    }
-
-    @Test
-    fun `should return common_letters`() {
-        var actual = findCommonLetters("fghij", "fguij")
-        assertEquals("fgij", actual)
-        actual = findCommonLetters("abcde", "axcye")
-        assertEquals("ace", actual)
-    }
-
-    private fun findCommonLetters(first: String, second: String): String {
-        val result = StringBuilder()
-        for (i in 0 until first.length) {
-            if (first[i] == second[i]) result += first[i]
-        }
-        return result.toString()
-    }
-
-    operator fun StringBuilder.plusAssign(c: Char) {
-        this.append(c)
-    }
-
-    @Test
-    fun `should find strings with single-letter difference`() {
-        val actual = hasSingleDifference("fghij", "fguij")
-        assertTrue(actual)
-        assertFalse(hasSingleDifference("abcde", "axcye"))
-        assertFalse(hasSingleDifference("abcde", "abcde"))
-        assertFalse(hasSingleDifference("", ""))
-    }
-
-    private fun hasSingleDifference(first: String, second: String): Boolean {
-        var diffs = 0
-        for (i in 0 until first.length) {
-            if (first[i] != second[i]) {
-                ++diffs
-            }
-            if (diffs >= 2) break
-        }
-        return diffs == 1
+        assertEquals("fgij", actualResult)
     }
 }

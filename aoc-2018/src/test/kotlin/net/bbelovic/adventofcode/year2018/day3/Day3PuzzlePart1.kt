c4d5314a49a73db13b557fd2c2bd838871ec0a34
bbelovic <bbelovic@gmail.com>
2018-12-17 22:21:39 +0100

    starting with day 3

---
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1.kt
@@ -0,0 +1,16 @@
+package net.bbelovic.adventofcode.year2018.day3
+
+import org.junit.jupiter.api.Test
+
+class Day3PuzzlePart1 {
+    @Test
+    fun `should compute rectangle intersection`() {
+        val first = Rectangle(1, 3, 4, 4)
+        val second = Rectangle(3, 1, 4, 4)
+
+        val area = first.intersect(second)
+
+
+
+    }
+}

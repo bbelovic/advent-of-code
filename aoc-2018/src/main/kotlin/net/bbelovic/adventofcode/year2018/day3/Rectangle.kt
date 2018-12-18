2ba6b8167449aa0e5c6107852fcdfbd045bdfc01
bbelovic <bbelovic@gmail.com>
2018-12-18 21:06:44 +0100

    Initial intersection implementation

---
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day3/Rectangle.kt
@@ -0,0 +1,13 @@
+package net.bbelovic.adventofcode.year2018.day3
+
+data class Rectangle(val upperLeftX: Int, val upperLeftY: Int, val width: Int, val height: Int) {
+    fun intersect(second: Rectangle): Int {
+        val x = upperLeftX..(upperLeftX + width)
+        val intersectX = x.intersect(second.upperLeftX..(second.upperLeftX + second.width))
+        val y = upperLeftY..(upperLeftY + height)
+        val intersectY = y.intersect(second.upperLeftY..(second.upperLeftY + second.height))
+
+        if (intersectX.isEmpty() || intersectY.isEmpty()) return 0
+        return (intersectX.size - 1) * (intersectY.size - 1)
+    }
+}
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1.kt
@@ -1,5 +1,6 @@
 package net.bbelovic.adventofcode.year2018.day3
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day3PuzzlePart1 {
@@ -7,10 +8,12 @@ class Day3PuzzlePart1 {
     fun `should compute rectangle intersection`() {
         val first = Rectangle(1, 3, 4, 4)
         val second = Rectangle(3, 1, 4, 4)
-
         val area = first.intersect(second)
+        Assertions.assertEquals(4, area)
+    }
 
-
-
+    @Test
+    fun `should parse rectangle from string`() {
+        val input = "#1 @ 1,3: 4x4"
     }
 }

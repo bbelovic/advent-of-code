d979c96102796c1e7c8040dc6b738549657a92f9
bbelovic <bbelovic@gmail.com>
2018-12-22 13:28:41 +0100

    improving tests round 2

--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1.kt
@@ -1,11 +1,10 @@
 package net.bbelovic.adventofcode.year2018.day3
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import java.util.regex.Pattern
 
 class Day3PuzzlePart1 {
     @Test
@@ -13,7 +12,7 @@ class Day3PuzzlePart1 {
         val first = Rectangle(1, 3, 4, 4)
         val second = Rectangle(3, 1, 4, 4)
         val area = first.intersect(second)
-        Assertions.assertEquals(4, area)
+        assertEquals(4, area)
     }
 
     @Test
@@ -21,31 +20,16 @@ class Day3PuzzlePart1 {
         val input = "#1 @ 1,3: 4x4"
         val expected: Rectangle = RectangleParser().parse(input)
         val actual = Rectangle(1,3, 4, 4)
-        Assertions.assertEquals(expected, actual)
+        assertEquals(expected, actual)
     }
 
     @ParameterizedTest
     @MethodSource("testData")
-    fun `should parse fabric information the Kotlin way`() {
-        val input = "#1 @ 1,3: 4x4"
+    fun `should parse fabric information the Kotlin way`(input: String, expected: List<Int>) {
         val regex = """#(\d{1,}) @ (\d{1,}),(\d{1,}): (\d{1,})x(\d{1,})""".toRegex()
         val (id, x, y, width, height) = regex.matchEntire(input)!!.destructured
-        val groupValues = regex.matchEntire(input)!!.groupValues
-        println(groupValues)
-
-        Assertions.assertEquals(1, id.toInt())
-        Assertions.assertEquals(1, x.toInt())
-        Assertions.assertEquals(3, y.toInt())
-        Assertions.assertEquals(4, width.toInt())
-        Assertions.assertEquals(4, height.toInt())
-
-        /*
-        #99 @ 335,901: 28x17
-        #100 @ 52,312: 14x16
-        #101 @ 13,487: 21x13
-        #102 @ 937,624: 15x23
-        */
-
+        val actual = listOf(id.toInt(), x.toInt(), y.toInt(), width.toInt(), height.toInt())
+        assertEquals(expected, actual)
     }
 
     companion object {
@@ -53,7 +37,9 @@ class Day3PuzzlePart1 {
         private fun testData() = listOf(
                 Arguments.of("#1 @ 1,3: 4x4", listOf(1, 1, 3, 4, 4)),
                 Arguments.of("#99 @ 335,901: 28x17", listOf(99, 335, 901, 28, 17)),
-                Arguments.of("#1 @ 1,3: 4x4"), listOf(1, 1, 3, 4, 4)
+                Arguments.of("#100 @ 52,312: 14x16", listOf(100, 52, 312, 14, 16)),
+                Arguments.of("#101 @ 13,487: 21x13", listOf(101, 13, 487, 21, 13)),
+                Arguments.of("#102 @ 937,624: 15x23", listOf(102, 937, 624, 15, 23))
         )
     }
 }

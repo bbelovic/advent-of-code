c596a7488b07d2cb10ee546f24a182fb66f81c42
bbelovic <bbelovic@gmail.com>
2018-12-23 21:15:22 +0100

    Implemented rectangle ID.

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day3/Rectangle.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day3/Rectangle.kt
@@ -1,6 +1,6 @@
 package net.bbelovic.adventofcode.year2018.day3
 
-data class Rectangle(val upperLeftX: Int, val upperLeftY: Int, val width: Int, val height: Int) {
+data class Rectangle(val id: Int, val upperLeftX: Int, val upperLeftY: Int, val width: Int, val height: Int) {
     fun intersect(second: Rectangle): Int {
         val x = upperLeftX..(upperLeftX + width)
         val intersectX = x.intersect(second.upperLeftX..(second.upperLeftX + second.width))
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1Test.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day3/Day3PuzzlePart1Test.kt
@@ -10,8 +10,8 @@ import org.junit.jupiter.params.provider.MethodSource
 class Day3PuzzlePart1Test {
     @Test
     fun `should compute rectangle intersection`() {
-        val first = Rectangle(1, 3, 4, 4)
-        val second = Rectangle(3, 1, 4, 4)
+        val first = Rectangle(1,1, 3, 4, 4)
+        val second = Rectangle(2,3, 1, 4, 4)
         val area = first.intersect(second)
         assertEquals(4, area)
     }
@@ -34,13 +34,13 @@ class Day3PuzzlePart1Test {
     companion object {
         @JvmStatic
         private fun testRectangles() = listOf(
-                Arguments { arrayOf("#1 @ 1,3: 4x4", Rectangle(1, 3, 4,4)) },
-                Arguments { arrayOf("#99 @ 335,901: 28x17", Rectangle(335, 901, 28, 17)) },
-                Arguments { arrayOf("#100 @ 52,312: 14x16", Rectangle( 52, 312, 14, 16)) },
-                Arguments { arrayOf("#101 @ 13,487: 21x13", Rectangle( 13, 487, 21, 13)) },
-                Arguments { arrayOf("#403 @ 201,97: 6x9", Rectangle(201, 97, 6, 9)) },
-                Arguments { arrayOf("#96 @ 8,593: 10x21", Rectangle(8, 593, 10, 21)) },
-                Arguments { arrayOf("#102 @ 937,624: 15x23", Rectangle(937, 624, 15, 23)) }
+                Arguments { arrayOf("#1 @ 1,3: 4x4", Rectangle(1,1, 3, 4, 4)) },
+                Arguments { arrayOf("#99 @ 335,901: 28x17", Rectangle(99,335, 901, 28, 17)) },
+                Arguments { arrayOf("#100 @ 52,312: 14x16", Rectangle(100, 52, 312, 14, 16)) },
+                Arguments { arrayOf("#101 @ 13,487: 21x13", Rectangle(101, 13, 487, 21, 13)) },
+                Arguments { arrayOf("#403 @ 201,97: 6x9", Rectangle(403,201, 97, 6, 9)) },
+                Arguments { arrayOf("#96 @ 8,593: 10x21", Rectangle(96,8, 593, 10, 21)) },
+                Arguments { arrayOf("#102 @ 937,624: 15x23", Rectangle(102,937, 624, 15, 23)) }
         )
     }
 }

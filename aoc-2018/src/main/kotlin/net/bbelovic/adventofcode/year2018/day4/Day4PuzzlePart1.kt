c23e36383592f895fe7186dadfa805207880cc34
bbelovic <bbelovic@gmail.com>
2019-08-25 21:56:30 +0200

    Puzzle 4 - beginning

---
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1.kt
@@ -0,0 +1,4 @@
+package net.bbelovic.adventofcode.year2018.day4
+
+class Day4PuzzlePart1 {
+}
---
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1Test.kt
@@ -0,0 +1,10 @@
+package net.bbelovic.adventofcode.year2018.day4
+
+import org.junit.jupiter.api.Test
+
+internal class Day4PuzzlePart1Test {
+    @Test
+    fun testSortInput(): Unit {
+
+    }
+}

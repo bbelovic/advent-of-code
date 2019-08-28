5ecb6287f644e871ac1dfe9f05decdd01686d318
bbelovic <bbelovic@gmail.com>
2019-08-28 21:51:18 +0200

    Puzzle 4 - working test for input sorting, round 1

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/InputLineComparator.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/InputLineComparator.kt
@@ -1,10 +1,20 @@
 package net.bbelovic.adventofcode.year2018.day4
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Comparator
 
 object InputLineComparator : Comparator<String> {
-    override fun compare(o1: String?, o2: String?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun compare(o1: String, o2: String): Int {
+        val first = parseDateAndTimeFromLine(o1)
+        val second = parseDateAndTimeFromLine(o2)
+        return first.compareTo(second)
     }
 
+    fun parseDateAndTimeFromLine(line: String): LocalDateTime {
+        val regex = """\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] ([a-zA-Z0-9# ]+)""".toRegex()
+        val (dateTimePart: String) = regex.matchEntire(line)!!.destructured
+        val temporalAccessor = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm").parse(dateTimePart)
+        return LocalDateTime.from(temporalAccessor)
+    }
 }
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1Test.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1Test.kt
@@ -13,13 +13,13 @@ internal class Day4PuzzlePart1Test {
                     [1518-06-10 00:37] wakes up
                     [1518-07-08 00:20] wakes up
                     [1518-05-13 00:24] falls asleep
-                    """.lineSequence()
+                    """.trimIndent().lineSequence()
         val expected ="""
             [1518-05-13 00:24] falls asleep
             [1518-06-10 00:37] wakes up
             [1518-07-08 00:20] wakes up
             [1518-08-06 00:13] falls asleep
-        """.lines()
+        """.trimIndent().lines()
 
         val actual = input.sortedWith(InputLineComparator).toList()
         Assertions.assertEquals(expected, actual)
@@ -27,17 +27,17 @@ internal class Day4PuzzlePart1Test {
 
     @Test
     fun testRegex() {
-        val regex = """(\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}\]) ([a-zA-Z0-9]+)""".toRegex()
+        val regex = """\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] ([a-zA-Z0-9# ]+)""".toRegex()
         val regex2 = """\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\]""".toRegex()
         val input = "[1518-09-28 23:59] Guard #3023 begins shift"
-        val input2 = "[1518-09-28 23:59]"
-        val (first: String) = regex.matchEntire(input)!!.destructured
+        val input2 = "[1518-09-28 23:59] falls asleep"
+        val (first: String, second: String) = regex.matchEntire(input2)!!.destructured
 
         val result = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm").parse(first)
         val dateTime = LocalDateTime.from(result)
 
 
 
-        println("${first}, ${dateTime}")
+        println("${first}, ${dateTime}, ${second}")
     }
 }

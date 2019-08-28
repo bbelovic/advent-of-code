cfa61182950a0c459aaa4e7b93e8100db24df728
bbelovic <bbelovic@gmail.com>
2019-08-28 21:28:55 +0200

    Puzzle 4 - test for input sorting

---
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/InputLineComparator.kt
@@ -0,0 +1,10 @@
+package net.bbelovic.adventofcode.year2018.day4
+
+import java.util.Comparator
+
+object InputLineComparator : Comparator<String> {
+    override fun compare(o1: String?, o2: String?): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1Test.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart1Test.kt
@@ -1,36 +1,43 @@
 package net.bbelovic.adventofcode.year2018.day4
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 internal class Day4PuzzlePart1Test {
     @Test
     fun testSortInput(): Unit {
-        val s = """[1518-08-06 00:13] falls asleep
+        val input = """
+                    [1518-08-06 00:13] falls asleep
                     [1518-06-10 00:37] wakes up
                     [1518-07-08 00:20] wakes up
                     [1518-05-13 00:24] falls asleep
-                    [1518-11-07 00:32] falls asleep
-                    [1518-11-21 00:01] Guard #641 begins shift
-                    [1518-05-29 00:31] falls asleep
-                    [1518-09-28 23:59] Guard #3023 begins shift
                     """.lineSequence()
+        val expected ="""
+            [1518-05-13 00:24] falls asleep
+            [1518-06-10 00:37] wakes up
+            [1518-07-08 00:20] wakes up
+            [1518-08-06 00:13] falls asleep
+        """.lines()
 
-
+        val actual = input.sortedWith(InputLineComparator).toList()
+        Assertions.assertEquals(expected, actual)
     }
 
     @Test
     fun testRegex() {
-        val regex = """([[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}]) ([a-zA-Z0-9]+)""".toRegex()
+        val regex = """(\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}\]) ([a-zA-Z0-9]+)""".toRegex()
         val regex2 = """\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\]""".toRegex()
         val input = "[1518-09-28 23:59] Guard #3023 begins shift"
         val input2 = "[1518-09-28 23:59]"
-        val (first: String) = regex2.matchEntire(input2)!!.destructured
+        val (first: String) = regex.matchEntire(input)!!.destructured
 
         val result = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm").parse(first)
+        val dateTime = LocalDateTime.from(result)
 
 
 
-        println("${first}, ${result}")
+        println("${first}, ${dateTime}")
     }
 }

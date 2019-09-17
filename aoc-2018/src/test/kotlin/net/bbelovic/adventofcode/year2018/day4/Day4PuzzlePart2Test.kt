ceb92cb35ac8928aaa575155a4505f699912df59
bbelovic <bbelovic@gmail.com>
2019-09-17 21:57:39 +0200

    Puzzle 4 - starting with part2

---
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart2Test.kt
@@ -0,0 +1,33 @@
+package net.bbelovic.adventofcode.year2018.day4
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class Day4PuzzlePart2Test {
+    @Test
+    fun `should process demo input`() {
+        val demoInput = """
+            [1518-11-01 00:00] Guard #10 begins shift
+            [1518-11-01 00:05] falls asleep
+            [1518-11-01 00:25] wakes up
+            [1518-11-01 00:30] falls asleep
+            [1518-11-01 00:55] wakes up
+            [1518-11-01 23:58] Guard #99 begins shift
+            [1518-11-02 00:40] falls asleep
+            [1518-11-02 00:50] wakes up
+            [1518-11-03 00:05] Guard #10 begins shift
+            [1518-11-03 00:24] falls asleep
+            [1518-11-03 00:29] wakes up
+            [1518-11-04 00:02] Guard #99 begins shift
+            [1518-11-04 00:36] falls asleep
+            [1518-11-04 00:46] wakes up
+            [1518-11-05 00:03] Guard #99 begins shift
+            [1518-11-05 00:45] falls asleep
+            [1518-11-05 00:55] wakes up"""
+                .trimIndent().lines().shuffled()
+        val puzzle = Day4PuzzlePart1(MostMinutesGuardAsleepSelector())
+        val actual = puzzle.solve(demoInput)
+        Assertions.assertEquals(4455, actual)
+
+    }
+}

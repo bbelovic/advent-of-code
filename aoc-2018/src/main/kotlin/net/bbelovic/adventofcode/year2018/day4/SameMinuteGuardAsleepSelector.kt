d4c1e7dcc6058aace2e39f0de52781ef941b3868
bbelovic <bbelovic@gmail.com>
2019-11-26 22:01:03 +0100

    came back with day 4 part 2

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
@@ -1,10 +1,13 @@
 package net.bbelovic.adventofcode.year2018.day4
 
+import java.util.*
+
 class SameMinuteGuardAsleepSelector : GuardAsleepSelector {
     override fun process(records: List<GuardRecord>): Int {
         val groupBy = records.groupBy { it.id }
         println(groupBy)
         val groupingBy = records.groupingBy { it.id }
+        groupingBy.reduce { key, accumulator, element -> accumulator.minutes.and(element.minutes) }
         println(groupingBy)
         return 0
     }

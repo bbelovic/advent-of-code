cbd845c596bfed72db9e0ef6c6a9277a5118b7f7
bbelovic <bbelovic@gmail.com>
2019-11-27 19:39:42 +0100

    came back with day 4 part 2 - continuing

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
@@ -6,8 +6,16 @@ class SameMinuteGuardAsleepSelector : GuardAsleepSelector {
     override fun process(records: List<GuardRecord>): Int {
         val groupBy = records.groupBy { it.id }
         println(groupBy)
+        val records99 = groupBy[99]!!
+        val acc = records99[0].minutes
+        for (i in 1 until records99.size) {
+            acc.and(records99[i].minutes)
+        }
+
+        println(acc)
+
         val groupingBy = records.groupingBy { it.id }
-        groupingBy.reduce { key, accumulator, element -> accumulator.minutes.and(element.minutes) }
+//        groupingBy.reduce { key, accumulator, element -> accumulator.minutes.and(element.minutes) }
         println(groupingBy)
         return 0
     }

ace8255f96204e88e1c036afcb5179719e1820b0
bbelovic <bbelovic@gmail.com>
2019-11-29 11:47:21 +0100

    day 4 part 2 - playing around with collections, sequences and similar things, both kotlin style and java style

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepSelector.kt
@@ -1,22 +1,23 @@
 package net.bbelovic.adventofcode.year2018.day4
 
-import java.util.*
+import java.util.stream.Collectors
+import java.util.stream.Stream
+
+data class IdAndMinutes(val id: Int, val minutes: Stream<Int>)
 
 class SameMinuteGuardAsleepSelector : GuardAsleepSelector {
     override fun process(records: List<GuardRecord>): Int {
-        val groupBy = records.groupBy { it.id }
-        println(groupBy)
-        val records99 = groupBy[99]!!
-        val acc = records99[0].minutes
-        for (i in 1 until records99.size) {
-            acc.and(records99[i].minutes)
-        }
+        val x = {record: GuardRecord -> record.minutes}
+        val y = {record: GuardRecord -> record.minutes.stream().boxed()}
+        records.asSequence().groupBy (GuardRecord::id , GuardRecord::minutes)
 
-        println(acc)
+        val m: Map<Int, List<Int>> = records.stream().collect(
+                Collectors.groupingBy({g: GuardRecord -> g.id}, Collectors.flatMapping(y, Collectors.toList<Int>())))
 
-        val groupingBy = records.groupingBy { it.id }
-//        groupingBy.reduce { key, accumulator, element -> accumulator.minutes.and(element.minutes) }
-        println(groupingBy)
+        print(m)
+
+
+
         return 0
     }
 }

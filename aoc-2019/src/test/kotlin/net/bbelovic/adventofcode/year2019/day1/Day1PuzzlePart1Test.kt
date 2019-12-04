ca33779981f4bad7b8bff8407b0fa71d8fb49257
bbelovic <bbelovic@gmail.com>
2019-12-04 21:30:05 +0100

    finished puzzle part 1 of day 1 for year 2019

--- aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day1/Day1PuzzlePart1Test.kt
+++ aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day1/Day1PuzzlePart1Test.kt
@@ -1,14 +1,17 @@
 package net.bbelovic.adventofcode.year2019.day1
 
+import net.bbelovic.adventofcode.InputReader
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day1PuzzlePart1Test {
     @Test
     fun `should compute total fuel`() {
-        val expectedFuel = 2 + 2 + 654 + 33583L
+        val puzzleInput = InputReader().readAllLines("input1.txt")
         val puzzlePart1 = Day1PuzzlePart1()
-        val actualFuel = puzzlePart1.solve(listOf("12", "14", "1969", "100756"))
+
+        val expectedFuel = 3210097L
+        val actualFuel = puzzlePart1.solve(puzzleInput)
         assertEquals(expectedFuel, actualFuel)
     }
 }

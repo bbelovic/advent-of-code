c2de0c050c7942c2b21785cc2008a66203849885
bbelovic <bbelovic@gmail.com>
2019-12-18 22:16:31 +0100

    polish

--- aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepGroupingSelector.kt
+++ aoc-2018/src/main/kotlin/net/bbelovic/adventofcode/year2018/day4/SameMinuteGuardAsleepGroupingSelector.kt
@@ -4,31 +4,23 @@ import kotlin.streams.toList
 
 class SameMinuteGuardAsleepGroupingSelector : GuardAsleepSelector {
     override fun process(records: List<GuardRecord>): Int {
-        val groupingBy = records.asSequence().groupingBy { record -> record.id }
-
-
-        val op = {key: Int, accumulator: MutableList<Int>?, el: GuardRecord, first: Boolean ->
-            val boxedMinutesStream = el.minutes.stream().boxed().toList()
-            if (first) {
-                val list = mutableListOf<Int>()
-                list.addAll(boxedMinutesStream)
-                list
-            } else {
-            accumulator!!.addAll(boxedMinutesStream)
-            accumulator
-            }
-        }
-
-        val round1 = groupingBy.aggregate(op)
-
-
-        val round2 = round1.mapValues { entry -> entry.value.groupingBy { list -> list }.eachCount() }
-        val round3 = round2.mapValues { entry -> entry.value.entries.map { e -> e.toPair() } }
-
-        val round4 = round3.mapValues { entry -> entry.value.asSequence().maxBy { pair -> pair.second } ?: Pair(0, 0)}
-
-
-        val (id, pair) = round4.entries.asSequence().maxBy { entry -> entry.value.second }!!
+        val (id, pair) = records.asSequence()
+                .groupingBy { record -> record.id }
+                .aggregate { _: Int, accumulator: MutableList<Int>?, el: GuardRecord, first: Boolean ->
+                    val boxedMinutesStream = el.minutes.stream().boxed().toList()
+                    if (first) {
+                        val list = mutableListOf<Int>()
+                        list.addAll(boxedMinutesStream)
+                        list
+                    } else {
+                        accumulator!!.addAll(boxedMinutesStream)
+                        accumulator
+                    }
+                }
+                .mapValues { entry -> entry.value!!.groupingBy { minutesAsleep -> minutesAsleep }.eachCount() }
+                .mapValues { entry -> entry.value.entries.map { e -> e.toPair() } }
+                .mapValues { entry -> entry.value.asSequence().maxBy { pair -> pair.second } ?: Pair(0, 0)}
+                .entries.asSequence().maxBy { entry -> entry.value.second }!!
         return id * pair.first
-    }
+}
 }
--- aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart2Test.kt
+++ aoc-2018/src/test/kotlin/net/bbelovic/adventofcode/year2018/day4/Day4PuzzlePart2Test.kt
@@ -3,6 +3,12 @@ package net.bbelovic.adventofcode.year2018.day4
 import net.bbelovic.adventofcode.InputReader
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+const val EXPECTED_RESULT_DEMO_INPUT = 4455
+const val EXPECTED_RESULT_PUZZLE_INPUT = 117061
 
 internal class Day4PuzzlePart2Test {
     @Test
@@ -27,45 +33,28 @@ internal class Day4PuzzlePart2Test {
             [1518-11-05 00:55] wakes up"""
                 .trimIndent().lines().shuffled()
         val puzzle = Day4PuzzlePart1(SameMinuteGuardAsleepSelector())
-        val expected = 4455
         val actual = puzzle.solve(demoInput)
-        assertEquals(expected, actual)
+        assertEquals(EXPECTED_RESULT_DEMO_INPUT, actual)
     }
 
-    @Test
-    fun `should process puzzle input`() {
-        val puzzle = Day4PuzzlePart1(SameMinuteGuardAsleepSelector())
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `should process puzzle input`(selector: GuardAsleepSelector) {
+        val puzzle = Day4PuzzlePart1(selector)
         val input = InputReader().readAllLines("input4.txt")
-        val expected = 117061
         val actual = puzzle.solve(input)
-        assertEquals(expected, actual)
+        assertEquals(EXPECTED_RESULT_PUZZLE_INPUT, actual)
     }
 
-    @Test
-    fun `should understand grouping`() {
-        val demoInput = """
-            [1518-11-01 00:00] Guard #10 begins shift
-            [1518-11-01 00:05] falls asleep
-            [1518-11-01 00:25] wakes up
-            [1518-11-01 00:30] falls asleep
-            [1518-11-01 00:55] wakes up
-            [1518-11-01 23:58] Guard #99 begins shift
-            [1518-11-02 00:40] falls asleep
-            [1518-11-02 00:50] wakes up
-            [1518-11-03 00:05] Guard #10 begins shift
-            [1518-11-03 00:24] falls asleep
-            [1518-11-03 00:29] wakes up
-            [1518-11-04 00:02] Guard #99 begins shift
-            [1518-11-04 00:36] falls asleep
-            [1518-11-04 00:46] wakes up
-            [1518-11-05 00:03] Guard #99 begins shift
-            [1518-11-05 00:45] falls asleep
-            [1518-11-05 00:55] wakes up"""
-                .trimIndent().lines().shuffled()
-        val puzzle = Day4PuzzlePart1(SameMinuteGuardAsleepGroupingSelector())
-        val expected = 4455
-        val actual = puzzle.solve(demoInput)
-        assertEquals(expected, actual)
-
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+                Arguments {
+                    arrayOf(SameMinuteGuardAsleepGroupingSelector())
+                },
+                Arguments {
+                    arrayOf(SameMinuteGuardAsleepSelector())
+                }
+        )
     }
 }

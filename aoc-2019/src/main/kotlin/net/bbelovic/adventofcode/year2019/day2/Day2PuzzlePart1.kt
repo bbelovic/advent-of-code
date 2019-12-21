dc2ddfa542d667923ab069a3de8aac87dadd977b
bbelovic <bbelovic@gmail.com>
2019-12-21 14:17:07 +0100

    day 2 - works on demo input

--- aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day2/Day2PuzzlePart1.kt
+++ aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day2/Day2PuzzlePart1.kt
@@ -1,5 +1,31 @@
 package net.bbelovic.adventofcode.year2019.day2
 
 class Day2PuzzlePart1 {
+    fun solve(input: MutableList<String>): String {
+        var pos = 0
+        var res = ""
+        while (res != "halt") {
+            when (input[pos]) {
+                // add two values, write on 3rd position, move forward
+                "1" -> {
+                    val first = input[pos + 1].toInt()
+                    val second = input[pos + 2].toInt()
+                    val resultPos = input[pos + 3].toInt()
+                    input[resultPos] = (input[first].toInt() + input[second].toInt()).toString()
+                    pos += 4; res = "add"
+                }
+                "2" -> {
+                    val first = input[pos + 1].toInt()
+                    val second = input[pos + 2].toInt()
+                    val resultPos = input[pos + 3].toInt()
+                    input[resultPos] = (input[first].toInt() * input[second].toInt()).toString()
+                    pos += 4; res = "add"
+                }
+                "99" -> res = "halt"
+                else -> res = "err"
+            }
+        }
+        return input.joinToString(separator = ",")
+    }
 
 }
--- aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day2/Day2PuzzlePart1Test.kt
+++ aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day2/Day2PuzzlePart1Test.kt
@@ -1,14 +1,26 @@
 package net.bbelovic.adventofcode.year2019.day2
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class Day2PuzzlePart1Test {
-    @Test
-    fun `should process input line`() {
-        val input = "1,0,0,0,99"
-        val expectedOutput = "2,0,0,0,99"
-        val actualOutput = Day2PuzzlePart1()
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun `should process input line with both opcodes`(input: String, expectedOutput: String) {
+        val parsedInput = input.split(""",""").toMutableList()
+        val actualOutput = Day2PuzzlePart1().solve(parsedInput)
         assertThat(actualOutput).isEqualTo(expectedOutput)
+    }
+
+    companion object {
+        @JvmStatic
+        fun testData() = listOf(
+                Arguments { arrayOf("1,0,0,0,99", "2,0,0,0,99")},
+                Arguments { arrayOf("2,3,0,3,99", "2,3,0,6,99")},
+                Arguments { arrayOf("2,4,4,5,99,0", "2,4,4,5,99,9801")},
+                Arguments { arrayOf("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")}
+        )
     }
 }

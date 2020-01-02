102f3b42013e2ede4c2c743bf8188ca9a2785c33
bbelovic <bbelovic@gmail.com>
2020-01-02 21:30:05 +0100

    part2 of day 3 of 2019 - part 2 running on demo input

--- aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day3/Day3PuzzlePart2.kt
+++ aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day3/Day3PuzzlePart2.kt
@@ -2,12 +2,27 @@ package net.bbelovic.adventofcode.year2019.day3
 
 class Day3PuzzlePart2(val space: Space) : AbstractDay3Puzzle(space) {
     override fun solve(input: List<List<String>>): Int {
-        val intersections = findIntersections(input)
+        val wire1Coordinates = space.move(input[0])
+        val wire2Coordinates = space.move(input[1])
+        val intersections = findIntersections(wire1Coordinates, wire2Coordinates)
         var result = Int.MAX_VALUE
         for (intersection in intersections) {
-            val sum = space.move(input[0], intersection) + space.move(input[1], intersection)
+            var steps1 = 0
+            var steps2 = 0
+            steps1 = wire1Coordinates.indexOf(intersection) + 1
+
+
+            steps2 = wire2Coordinates.indexOf(intersection) + 1
+
+            val sum = steps1 + steps2
             if (sum < result) result = sum
         }
         return result
+    }
+
+    private fun findIntersections(wire1Coordinates: MutableSet<Coordinates>, wire2Coordinates: MutableSet<Coordinates>): MutableSet<Coordinates> {
+        val mutableSet = wire1Coordinates.toMutableSet()
+        mutableSet.retainAll(wire2Coordinates)
+        return mutableSet
     }
 }

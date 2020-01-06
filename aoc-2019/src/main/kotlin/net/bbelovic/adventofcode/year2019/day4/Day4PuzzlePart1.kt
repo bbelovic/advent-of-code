61c9581c742f76ada46d1b510da0467313b50e50
bbelovic <bbelovic@gmail.com>
2020-01-06 21:42:14 +0100

    part1 of day 4 of 2019 - works on demo input

--- aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day4/Day4PuzzlePart1.kt
+++ aoc-2019/src/main/kotlin/net/bbelovic/adventofcode/year2019/day4/Day4PuzzlePart1.kt
@@ -4,7 +4,26 @@ import net.bbelovic.adventofcode.Puzzle
 
 class Day4PuzzlePart1 : Puzzle<String, Int>{
     override fun solve(input: String): Int {
-        return 0
+        return if (checkNumber(input.toInt())) 1 else 0
+    }
+
+    private fun checkNumber(input: Int): Boolean {
+        var inputCopy = input
+        var init = 10
+        var act: Int
+        var descending = true
+        var hasDouble = false
+        do {
+            act = inputCopy % 10
+            descending = descending && (act <= init)
+            if (!hasDouble) {
+                hasDouble = (act == init)
+            }
+            inputCopy /= 10
+            init = act
+        } while (inputCopy != 0)
+//        println("$hasDouble - $descending")
+        return hasDouble && descending
     }
 
 }
--- aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day4/Day4PuzzlePart1Test.kt
+++ aoc-2019/src/test/kotlin/net/bbelovic/adventofcode/year2019/day4/Day4PuzzlePart1Test.kt
@@ -1,6 +1,5 @@
 package net.bbelovic.adventofcode.year2019.day4
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -10,31 +9,62 @@ import org.junit.jupiter.params.provider.MethodSource
 internal class Day4PuzzlePart1Test {
     @ParameterizedTest
     @MethodSource("testData")
-    fun `should test password properties`(password: String, matchCriteria: Boolean) {
+    fun `should test password properties`(password: String, matchCriteria: Int) {
         val actualCount = Day4PuzzlePart1().solve(password)
-        assertThat(actualCount).isEqualTo(1)
+        assertThat(actualCount).isEqualTo(matchCriteria)
     }
 
     companion object {
         @JvmStatic
-        fun testData() = listOf<Arguments>(
-                Arguments { arrayOf("111111", true) },
-                Arguments { arrayOf("223450", false) },
-                Arguments { arrayOf("123789", false) }
+        fun testData() = listOf(
+                Arguments { arrayOf("111111", 1) },
+                Arguments { arrayOf("223450", 0) },
+                Arguments { arrayOf("123789", 0) }
         )
     }
 
     @Test
     fun `test get digits`() {
-        var input = 123789
-        val expected = mutableListOf(1, 2, 3, 7, 8 ,9)
+        var input = 1233789
+        val expected = mutableListOf(1, 2, 3, 3, 7, 8 ,9)
         val actual = mutableListOf<Int>()
 
+        var init = 9
+        var act: Int
+        var descending = true
+        var hasDouble = false
         do {
-            actual.add(input % 10)
+            act = input % 10
+            descending = descending && (act <= init)
+            if (!hasDouble) {
+                hasDouble = (act == init)
+            }
+            actual.add(act)
             input /= 10
+
+            init = act
         } while (input != 0)
         actual.reverse()
         assertThat(actual).isEqualTo(expected)
+        assertThat(hasDouble && descending).isTrue()
+    }
+
+    private fun checkNumber(input: Int): Boolean {
+        var inputCopy = input
+        var init = 10
+        var act: Int
+        var descending = true
+        var hasDouble = false
+        do {
+            act = inputCopy % 10
+            descending = descending && (act <= init)
+            if (!hasDouble) {
+                hasDouble = (act == init)
+            }
+            inputCopy /= 10
+            init = act
+        } while (inputCopy != 0)
+//        println("$hasDouble - $descending")
+        return hasDouble && descending
     }
 }

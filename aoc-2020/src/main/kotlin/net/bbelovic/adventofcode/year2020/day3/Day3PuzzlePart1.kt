9b4b6e63e4fec957789e9362e8b28879eec0538e
bbelovic <bbelovic@gmail.com>
2021-01-01 22:02:37 +0100

    Refactored day 3 part 1 to use range in for loop

--- aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day3/Day3PuzzlePart1.kt
+++ aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day3/Day3PuzzlePart1.kt
@@ -4,17 +4,22 @@ class Day3PuzzlePart1 {
     fun solve(input: List<String>): Int {
         var treeCount = 0
         var idx = input[0].indexOf('.')
-        for (line in input) {
-            var copiedLine = line
-            if (idx > line.length) {
-                val mult = idx / line.length
-                copiedLine = line.repeat(mult + 1)
-            }
+
+        for (i in input.indices step 1) {
+            val copiedLine = repeatIfNeeded(idx, input[i])
             if (copiedLine[idx] == '#') {
                 ++treeCount
             }
             idx += 3
+
         }
         return treeCount
+    }
+
+    private fun repeatIfNeeded(idx: Int, line: String): String {
+        if (idx > line.length) {
+            return line.repeat((idx / line.length) + 1)
+        }
+        return line
     }
 }

f457bf4385f5f86d5a3f44d26a711df5bb1c0d1f
bbelovic <bbelovic@gmail.com>
2021-01-09 21:20:34 +0100

    Crafting regexes for day 4 part 2 puzzle

--- aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2Test.kt
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2Test.kt
@@ -1,5 +1,7 @@
 package net.bbelovic.adventofcode.year2020.day4
 
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class Day4PuzzlePart2Test {
@@ -20,22 +22,47 @@ iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"""
 
         val collectedKeys = mutableSetOf<String>()
         "eyr:2029 ecl:blu cid:129 byr:1989".splitToSequence(" ")
-            .map { each -> """([a-z]{3}):(.*)""".toRegex().matchEntire(each)!!.destructured.component1() }
+            .map { each -> processElement(each) }
             .toCollection(collectedKeys)
     }
 
-    fun processElement(element: String): Unit {
-        val rules = mapOf("pid" to """[0-9]{9}""".toRegex())
+
+//    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+//    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+//    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+//    hgt (Height) - a number followed by either cm or in:
+//    If cm, the number must be at least 150 and at most 193.
+//    If in, the number must be at least 59 and at most 76.
+//    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+//    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+//    pid (Passport ID) - a nine-digit number, including leading zeroes.
+//    cid (Country ID) - ignored, missing or not.
+
+    private fun processElement(element: String): String {
+        val rules = mapOf(
+            "byr" to """19[2-9][0-9]|200[0-2]""".toRegex(),
+            "hgt" to """1[5-9][0-3]cm|[5-7][9|0-6]in""".toRegex(),
+            "pid" to """[0-9]{9}""".toRegex(),
+
+            )
 
         val (key, value) = """([a-z]{3}):(.*)""".toRegex().matchEntire(element)!!.destructured
-        if (key == "pid") {
-            val regex = rules[key]!!
-            value.matches(regex)
+        return if (value.matches(rules[key]!!)) key else "invalid [$value] for [$key]"
+    }
+
+    @Test
+    fun testByr() {
+        val regex = """19[2-9][0-9]|200[0-2]""".toRegex()
+
+        for (i in 1920..2002) {
+            assertTrue(i.toString().matches(regex))
+        }
+        for (i in 0..1919) {
+            assertFalse(i.toString().matches(regex), "$i matched regex, but it should not")
         }
 
-        val result = when (key) {
-            "pid" -> if (value.matches(rules[key]!!)) key else "na"
-            else -> "invalid"
+        for (i in 2003..99999) {
+            assertFalse(i.toString().matches(regex), "$i matched regex, but it should not")
         }
     }
 }

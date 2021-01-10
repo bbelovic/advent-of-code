ffccca317c4dab49673e0aa0e6a5f6e957aba635
bbelovic <bbelovic@gmail.com>
2021-01-10 18:17:28 +0100

    Working implementation for day 4 puzzle part 2

--- aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2.kt
+++ aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2.kt
@@ -1,16 +1,24 @@
 package net.bbelovic.adventofcode.year2020.day4
 
+import java.lang.StringBuilder
+
 class Day4PuzzlePart2 {
     fun solve(lines: List<String>): Long {
         var cnt = 0L
         val collectedKeys = mutableSetOf<String>()
+        val sb = StringBuilder()
         for (line in lines) {
             if (line != "") {
+                sb.append("$line ")
                 line.splitToSequence(" ")
                     .map { each -> processElement(each) }
                     .toCollection(collectedKeys)
             } else {
-                cnt += if (collectedKeys.containsAll(setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))) +1 else 0
+                cnt += if (collectedKeys.containsAll(setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))) {+1} else {
+                    println("Invalid passport: $sb")
+                    0
+                }
+                sb.clear()
                 collectedKeys.clear()
             }
         }
@@ -26,7 +34,7 @@ class Day4PuzzlePart2 {
             "byr" to """19[2-9][0-9]|200[0-2]""".toRegex(),
             "iyr" to """201[0-9]|2020""".toRegex(),
             "eyr" to """202[0-9]|2030""".toRegex(),
-            "hgt" to """1[5-8][0-9]cm|19[0-3]cm|[5-7][9|0-6]in""".toRegex(),
+            "hgt" to """1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in""".toRegex(),
             "hcl" to """#[0-9a-f]{6}""".toRegex(),
             "ecl" to """amb|blu|brn|gry|grn|hzl|oth""".toRegex(),
             "pid" to """[0-9]{9}""".toRegex(),
--- aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2Test.kt
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day4/Day4PuzzlePart2Test.kt
@@ -3,7 +3,6 @@ package net.bbelovic.adventofcode.year2020.day4
 import net.bbelovic.adventofcode.InputReader
 import net.bbelovic.adventofcode.utils.dsl.testDataDsl
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -20,7 +19,7 @@ class Day4PuzzlePart2Test {
     fun `should count the number of valid passports`() {
         val inputLines = InputReader().readAllLines("input4.txt")
         val actual = Day4PuzzlePart2().solve(inputLines)
-        assertThat(actual).isEqualTo(0)
+        assertThat(actual).isEqualTo(109)
     }
 
     companion object {
@@ -53,58 +52,6 @@ ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
 hgt:59cm ecl:zzz
 eyr:2038 hcl:74454a iyr:2023
 pid:3556412378 byr:2007""", 0)
-        }
-    }
-
-//    byr (Birth Year) - four digits; at least 1920 and at most 2002.
-//    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-//    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-//    hgt (Height) - a number followed by either cm or in:
-//    If cm, the number must be at least 150 and at most 193.
-//    If in, the number must be at least 59 and at most 76.
-//    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-//    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-//    pid (Passport ID) - a nine-digit number, including leading zeroes.
-//    cid (Country ID) - ignored, missing or not.
-
-    private fun processElement(element: String): String {
-        val rules = mapOf(
-            "byr" to """19[2-9][0-9]|200[0-2]""".toRegex(),
-            "iyr" to """201[0-9]|2020""".toRegex(),
-            "eyr" to """202[0-9]|2030""".toRegex(),
-            "hgt" to """1[5-8][0-9]cm|19[0-3]cm|[5-7][9|0-6]in""".toRegex(),
-            "hcl" to """#[0-9a-f]{6}""".toRegex(),
-            "ecl" to """amb|blu|brn|gry|grn|hzl|oth""".toRegex(),
-            "pid" to """[0-9]{9}""".toRegex(),
-            "cid" to """.*""".toRegex()
-
-            )
-
-        val (key, value) = """([a-z]{3}):(.*)""".toRegex().matchEntire(element)!!.destructured
-        return if (value.matches(rules[key]!!)) key else "invalid value [$value] for key [$key]"
-    }
-
-    @Test
-    fun `test regex`(): Unit {
-        val actual = processElement("iyr:2015")
-        assertEquals("iyr", actual)
-
-        assertTrue("cid".matches(""".*""".toRegex()))
-    }
-
-    @Test
-    fun testByr() {
-        val regex = """19[2-9][0-9]|200[0-2]""".toRegex()
-
-        for (i in 1920..2002) {
-            assertTrue(i.toString().matches(regex))
-        }
-        for (i in 0..1919) {
-            assertFalse(i.toString().matches(regex), "$i matched regex, but it should not")
-        }
-
-        for (i in 2003..99999) {
-            assertFalse(i.toString().matches(regex), "$i matched regex, but it should not")
         }
     }
 }

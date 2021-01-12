23d2e8fb9ce94cd9adbc4952c70c76e2b4ae26a3
bbelovic <bbelovic@gmail.com>
2021-01-12 22:23:09 +0100

    Wrote test for day 5 part 1

---
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day5/Day5PuzzlePart1Test.kt
@@ -0,0 +1,13 @@
+package net.bbelovic.adventofcode.year2020.day5
+
+import org.junit.jupiter.api.Test
+
+class Day5PuzzlePart1Test {
+    @Test
+    fun `should find highest seat id on a boarding pass - example data`() {
+        val input = "FBFBBFFRLR"
+        val expectedSeatId = 357
+
+
+    }
+}

09e352c263f2fa0a5572dc7f7a15d7277d3c3400
bbelovic <bbelovic@gmail.com>
2021-01-13 21:39:41 +0100

    Working prototype for day 5 puzzle part 1

--- aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day5/Day5PuzzlePart1Test.kt
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day5/Day5PuzzlePart1Test.kt
@@ -1,5 +1,6 @@
 package net.bbelovic.adventofcode.year2020.day5
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day5PuzzlePart1Test {
@@ -8,6 +9,40 @@ class Day5PuzzlePart1Test {
         val input = "FBFBBFFRLR"
         val expectedSeatId = 357
 
+        var l = 0
+        var u = 127
+        var row = 0
+        val rowDirections = input.substring(0, 7)
 
+        for (i in  rowDirections.toList()) {
+            val m = (l + u) / 2
+            if (i == 'F') {
+                u = m
+                row = u
+            } else {
+                l = m + 1
+                row = l
+            }
+
+        }
+        assertThat(row).isEqualTo(44)
+
+        val colDirections = input.substring(7)
+        l = 0
+        u = 7
+        var col = 0
+        for (i in colDirections.toList()) {
+            val m = (l + u) / 2
+            if (i == 'L') {
+                u = m
+                col = u
+            } else {
+                l = m + 1
+                col = l
+            }
+        }
+        assertThat(col).isEqualTo(5)
+
+        assertThat(row * 8 + col).isEqualTo(expectedSeatId)
     }
 }

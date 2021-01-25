f1755132d7963272e7b4a18d1847159b8b0fd1f1
bbelovic <bbelovic@gmail.com>
2021-01-25 21:22:42 +0100

    Implemented day6 part 1 on full input data

--- aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart1Test.kt
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart1Test.kt
@@ -1,5 +1,6 @@
 package net.bbelovic.adventofcode.year2020.day6
 
+import net.bbelovic.adventofcode.InputReader
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -23,5 +24,12 @@ a
 b"""
         val actual = Day6PuzzlePart1().solve(input.lines())
         assertThat(actual).isEqualTo(11)
+    }
+
+    @Test
+    fun `should count yes answers`() {
+        val lines = InputReader().readAllLines("input6.txt")
+        val actual = Day6PuzzlePart1().solve(lines)
+        assertThat(actual).isEqualTo(6530L)
     }
 }

042c7819e22d722728a7a8b9882289f0b4ac9c17
bbelovic <bbelovic@gmail.com>
2021-01-27 20:45:17 +0100

    Day 6 part 2 puzzle implemented

--- aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart2.kt
+++ aoc-2020/src/main/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart2.kt
@@ -6,16 +6,19 @@ class Day6PuzzlePart2 : Puzzle<List<String>, Long> {
     override fun solve(input: List<String>): Long {
         var sum = 0L
         val c = mutableSetOf<Char>()
+        var lineNo = 0
         for (line in input) {
             if (line != "") {
-                if (c.isEmpty()) {
+                ++lineNo
+                if (c.isEmpty() && lineNo == 1) {
                     line.toCollection(c)
                 } else {
-                    c.removeAll(line.toList())
+                    c.retainAll(line.toList())
                 }
             } else {
                 sum += c.size
                 c.clear()
+                lineNo = 0
             }
         }
         sum += c.size
--- aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart2Test.kt
+++ aoc-2020/src/test/kotlin/net/bbelovic/adventofcode/year2020/day6/Day6PuzzlePart2Test.kt
@@ -1,5 +1,6 @@
 package net.bbelovic.adventofcode.year2020.day6
 
+import net.bbelovic.adventofcode.InputReader
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -25,5 +26,12 @@ b"""
 
         val actual = Day6PuzzlePart2().solve(input.lines())
         assertThat(actual).isEqualTo(6L)
+    }
+
+    @Test
+    fun `should count number of question answered by everyone`() {
+        val lines = InputReader().readAllLines("input6.txt")
+        val actual = Day6PuzzlePart2().solve(lines)
+        assertThat(actual).isEqualTo(3323L)
     }
 }

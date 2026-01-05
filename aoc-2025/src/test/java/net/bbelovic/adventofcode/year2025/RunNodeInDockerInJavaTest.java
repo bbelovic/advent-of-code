8cd89b78e810808b703a1fc78a1e5da7f574a913
Boris Belovic <bbelovic@gmail.com>
2026-01-05 21:46:28 +0100

    Using testcontainers to setup node

--- aoc-2025/src/test/java/net/bbelovic/adventofcode/year2025/RunNodeInDockerInJavaTest.java
+++ aoc-2025/src/test/java/net/bbelovic/adventofcode/year2025/RunNodeInDockerInJavaTest.java
@@ -1,4 +1,24 @@
 package net.bbelovic.adventofcode.year2025;
 
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
 public class RunNodeInDockerInJavaTest {
+    @Container
+    GenericContainer container = new GenericContainer(
+            new ImageFromDockerfile()
+                    .withFileFromClasspath("Dockerfile", "resources/"));
+
+    @Test
+    void testBuildFromDockerFile() {
+        Assertions.assertThat(container.isRunning()).isTrue();
+
+    }
+
 }

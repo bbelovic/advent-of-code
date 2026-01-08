c8b044ae571236ddab05b50b77cf081648b3928d
Boris Belovic <bbelovic@gmail.com>
2026-01-08 21:31:46 +0100

    Trying to tap container output

--- aoc-2025/src/test/java/net/bbelovic/adventofcode/year2025/RunNodeInDockerInJavaTest.java
+++ aoc-2025/src/test/java/net/bbelovic/adventofcode/year2025/RunNodeInDockerInJavaTest.java
@@ -2,13 +2,18 @@ package net.bbelovic.adventofcode.year2025;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class RunNodeInDockerInJavaTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RunNodeInDockerInJavaTest.class);
+
     @Container
     GenericContainer container = new GenericContainer(
             new ImageFromDockerfile()
@@ -20,6 +25,9 @@ public class RunNodeInDockerInJavaTest {
 
     @Test
     void testBuildFromDockerFile() {
+        Slf4jLogConsumer slf4jLogConsumer = new Slf4jLogConsumer(LOGGER).withSeparateOutputStreams();
+        container.followOutput(slf4jLogConsumer);
+
         Assertions.assertThat(container.isRunning()).isTrue();
 
     }
